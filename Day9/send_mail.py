682c5addb2f8aee04b61a725aeab83c259310d91
Abderrahim soumer <soumer120@gmail.com>
2020-03-29 18:54:59 +0100

    Read inbox unseen email

--- Day9/send_mail.py
+++ Day9/send_mail.py
@@ -3,11 +3,11 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
 # envirement variable
-username = 'exemple@gmail.com'
+username = 'abderrahimsoumer@gmail.com'
 password = 'password'
 
 def send_mail(text='Email Body', subject='Hello world',
-from_email='Abderrahim soumer<exemple@gmail.com>', to_emails= None, html=None):
+from_email='Abderrahim soumer<abderrahimsoumer@gmail.com>', to_emails= None, html=None):
     assert isinstance(to_emails, list)
     msg = MIMEMultipart('alternative')
     msg['From'] = username
