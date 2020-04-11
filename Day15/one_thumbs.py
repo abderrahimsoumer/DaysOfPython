73b70b904335dc28536709ef01ad1da23cbe9b19
Abderrahim soumer <soumer120@gmail.com>
2020-04-11 15:06:48 +0100

    Create thumbnail every half second in video

--- Day15/one_thumbs.py
+++ Day15/one_thumbs.py
@@ -6,20 +6,25 @@ from PIL import Image
 source_path = os.path.join(SAMPLE_INPUTS, 'sample.mp4')
 thumbnails_dir = os.path.join(SAMPLE_OUTPUTS, 'thumbnails')
 thumbnails_per_frame_dir = os.path.join(SAMPLE_OUTPUTS, 'thumbnails-per-frame')
+thumbnails_per_half_second_dir = os.path.join(SAMPLE_OUTPUTS, 'thumbnails-per-half-second')
 os.makedirs(thumbnails_dir, exist_ok=True)
 os.makedirs(thumbnails_per_frame_dir, exist_ok=True)
+os.makedirs(thumbnails_per_half_second_dir, exist_ok=True)
 
 clip = VideoFileClip(source_path)
 
-print(clip.reader.fps) # frame per second
-print(clip.reader.nframes)
-print(clip.duration) # in second
+fbs = clip.reader.fps # frame per second
+nframes = clip.reader.nframes
+duration = clip.duration # in second
 # duration = clip.duration
 # max_duration = int(duration) + 1
 
 for i, frame in enumerate(clip.iter_frames()):
     #frame = clip.get_frame(i) # np.array numpy array #inference
     #print(frame)
-    new_image_filepath = os.path.join(thumbnails_per_frame_dir, f"{i}.jpg")
-    new_image = Image.fromarray(frame)
-    new_image.save(new_image_filepath)
+    half_fbs = int(fbs / 2)
+    if i % half_fbs == 0:
+        current_ms = int((i / fbs) * 1000)
+        new_image_filepath = os.path.join(thumbnails_per_half_second_dir, f"{current_ms}.jpg")
+        new_image = Image.fromarray(frame)
+        new_image.save(new_image_filepath)
