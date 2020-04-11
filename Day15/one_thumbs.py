7c11c1c5b3b7b9239faa191d08d9ee0b13d2beb9
Abderrahim soumer <soumer120@gmail.com>
2020-04-11 14:53:03 +0100

    Create thumbnail every frame in video

--- Day15/one_thumbs.py
+++ Day15/one_thumbs.py
@@ -5,19 +5,21 @@ from PIL import Image
 
 source_path = os.path.join(SAMPLE_INPUTS, 'sample.mp4')
 thumbnails_dir = os.path.join(SAMPLE_OUTPUTS, 'thumbnails')
-os.makedirs(thumbnails_dir)
+thumbnails_per_frame_dir = os.path.join(SAMPLE_OUTPUTS, 'thumbnails-per-frame')
+os.makedirs(thumbnails_dir, exist_ok=True)
+os.makedirs(thumbnails_per_frame_dir, exist_ok=True)
 
 clip = VideoFileClip(source_path)
 
 print(clip.reader.fps) # frame per second
 print(clip.reader.nframes)
 print(clip.duration) # in second
-duration = clip.duration
-max_duration = int(duration) + 1
-for i in range(0, max_duration):
-    print(f"Frame i {i} second")
-    frame = clip.get_frame(i) # np.array numpy array #inference
+# duration = clip.duration
+# max_duration = int(duration) + 1
+
+for i, frame in enumerate(clip.iter_frames()):
+    #frame = clip.get_frame(i) # np.array numpy array #inference
     #print(frame)
-    new_image_filepath = os.path.join(thumbnails_dir, f"{i}.jpg")
+    new_image_filepath = os.path.join(thumbnails_per_frame_dir, f"{i}.jpg")
     new_image = Image.fromarray(frame)
     new_image.save(new_image_filepath)
