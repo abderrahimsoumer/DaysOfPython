94a3e410ed232e17bf417abae8897f9d733758bb
Abderrahim soumer <soumer120@gmail.com>
2020-04-11 11:35:04 +0100

    Finish Day 14
    - working with pipenv
    - working with flask
    - working with fastapi

--- Day14/server1.py
+++ Day14/server1.py
@@ -1,12 +1,25 @@
 from flask import Flask
 
+from logger import trigger_log_save
+from scrape import run as scrape_runner
+
 app = Flask(__name__)
 
-# declare the endpoint url for the function
-@app.route('/', methods=['GET'])
+# http://localhost:8000/
+@app.route("/", methods=['GET'])
 def hello_world():
-    return "This is a message says Hello world"
+    # run other code here.
+    return "Hello, world. this is Flask"
 
+# http://localhost:8000/abc
+@app.route("/abc", methods=['GET'])
+def abc_view():
+    # run other code here.
+    return "Hello, world. this is abc"
 
-if __name__ == '__main__':
-    app.run()
+@app.route("/box-office-mojo-scraper", methods=['POST'])
+def box_office_scraper_view():
+    # run other code here.
+    trigger_log_save()
+    scrape_runner()
+    return {"data": [1,2,3]}
